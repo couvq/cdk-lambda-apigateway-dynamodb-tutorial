77f89e13a0e96c52c6f6853fe23fe361c4c53439
couvq <quentincouvelaire@gmail.com>
2023-03-05 17:47:03 -0600

    added a new dynamodb table resource through our cdk package

--- lib/cdk-lambda-apigateway-dynamodb-tutorial-stack.ts
+++ lib/cdk-lambda-apigateway-dynamodb-tutorial-stack.ts
@@ -1,8 +1,8 @@
 import * as apigateway from "@aws-cdk/aws-apigateway";
 import * as lambda from "@aws-cdk/aws-lambda";
-import { Runtime } from "@aws-cdk/aws-lambda";
 import * as cdk from "@aws-cdk/core";
 import * as path from "path";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
 
 export class CdkLambdaApigatewayDynamodbTutorialStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -10,14 +10,18 @@ export class CdkLambdaApigatewayDynamodbTutorialStack extends cdk.Stack {
 
     const apiGateway = new apigateway.RestApi(this, "APIGateway", {});
 
+    new dynamodb.Table(this, "Users", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+    });
+
     const lambdaListUsers = new lambda.Function(this, "lambdaListUsers", {
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: "index.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "lambda-handler"))
+      code: lambda.Code.fromAsset(path.join(__dirname, "lambda-handler")),
     });
 
     apiGateway.root
-    .addResource('users')
-    .addMethod('GET', new apigateway.LambdaIntegration(lambdaListUsers));
+      .addResource("users")
+      .addMethod("GET", new apigateway.LambdaIntegration(lambdaListUsers));
   }
 }
