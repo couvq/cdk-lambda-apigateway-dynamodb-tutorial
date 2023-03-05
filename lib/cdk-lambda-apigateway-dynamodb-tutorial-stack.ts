372a033b551631b618675bff5a15813c383a3510
couvq <quentincouvelaire@gmail.com>
2023-03-05 16:27:05 -0600

    configured a lambda function to execute when our get users api gateway endpoint is hit

--- lib/cdk-lambda-apigateway-dynamodb-tutorial-stack.ts
+++ lib/cdk-lambda-apigateway-dynamodb-tutorial-stack.ts
@@ -1,6 +1,8 @@
-import * as cdk from "@aws-cdk/core";
-import { Construct } from "constructs";
 import * as apigateway from "@aws-cdk/aws-apigateway";
+import * as lambda from "@aws-cdk/aws-lambda";
+import { Runtime } from "@aws-cdk/aws-lambda";
+import * as cdk from "@aws-cdk/core";
+import * as path from "path";
 
 export class CdkLambdaApigatewayDynamodbTutorialStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -8,5 +10,14 @@ export class CdkLambdaApigatewayDynamodbTutorialStack extends cdk.Stack {
 
     const apiGateway = new apigateway.RestApi(this, "APIGateway", {});
     apiGateway.root.addMethod('GET');
+    const usersResource = apiGateway.root.addResource('users');
+
+    const lambdaListUsers = new lambda.Function(this, "lambdaListUsers", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset(path.join(__dirname, "lambda-handler"))
+    });
+
+    usersResource.addMethod('GET', new apigateway.LambdaIntegration(lambdaListUsers));
   }
 }
